9d90481afbff8bc2c317fb5d2432a27605730d57
mehdicode <kalan.mahdi@yahoo.com>
2017-12-20 20:55:12 -0800

    new layout

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,16 @@
-anime({
-    targets: 'img.bLogo',
-    translateY: [
-        { value: 0, duration: 1000 },
-        { value: 0, duration: 800 }
-      ],
-      rotate:{
-          value:'1turn',
-          easing: 'easeInOutSine',
+// anime({
+//     targets: 'img.bLogo',
+//     translateY: [
+//         { value: 0, duration: 1000 },
+//         { value: 0, duration: 800 }
+//       ],
+//       rotate:{
+//           value:'1turn',
+//           easing: 'easeInOutSine',
         
-      },
-      loop:true
-});
+//       },
+//       loop:true
+// });
 
 
 
