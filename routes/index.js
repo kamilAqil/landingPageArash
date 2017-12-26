e31b73e1aa3dd0ee2011db30da252ce0c749ef42
mehdicode <kalan.mahdi@yahoo.com>
2017-12-26 00:29:26 -0800

    confirm page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'BitDiem' });
 });
 
+router.get('/confirm', function(req, res, next) {
+  res.render('confirm', { title: 'Thank You' });
+});
+
 module.exports = router;
